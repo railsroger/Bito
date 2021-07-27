6e7c388d514fedf0ab42b2b10f7b5352fbc2ce23
RAILSROGER <RAILSROGER@GMAIL.COM>
2021-07-27 11:27:47 +0300

    Setting devise and create username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :authentication_keys => [:username]
 end
