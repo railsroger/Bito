6fecb14cdb1619a66954b0a51b5eea2902c1a163
RAILSROGER <RAILSROGER@GMAIL.COM>
2021-07-27 16:44:12 +0300

    Create functional registration for users and fixing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::API
 	acts_as_token_authentication_handler_for User
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   acts_as_token_authenticatable
   validates :email, uniqueness: true
-  validates :username, uniqueness: true
+  validates :username, presence: true, length: {maximum: 50}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :authentication_keys => [:username]
+         :recoverable, :rememberable, :validatable
 end
